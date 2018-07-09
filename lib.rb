c697232dbcbedae801d7a4f7dce470649df979c3
Cardell King <cardellking@Cardells-MacBook-Air.local>
2018-07-09 16:51:08 -0500

    generating secert code

--- lib.rb
+++ lib.rb
@@ -1,14 +1,17 @@
 class Mastermind
   def initialize
     @color_options = ["r", "b", "g", "y", "o", "p"]
-    @
+    @secret_code = []
+  end
+
+  def generate_code
+    @secret_code = @color_options.sample(4)
   end
 
   def play_game
     system ('clear')
     puts "Here are your options: #{@color_options}"
-
-
-
+    generate_code
   end
+
 end
